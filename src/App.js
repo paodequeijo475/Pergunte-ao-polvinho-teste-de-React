ca52e9e857b2e393ea9406d564f1311fe74cf2ce
Abner <abneroninjadassombras@gmail.com>
2022-07-04 21:56:57 -0300

    mudanca de fundo

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
 import './App.css';
-import React from 'react';
 
 function App() {
   var numero = 0
   function funcoes () {
-    adivinha()
-    contagemlick()
-  };
- function carregar () {
-    let corpo = document.getElementById('App')
-    let imagem = document.getElementById('imagemaleatoria')
-    let msg = document.getElementById('msg')
-    let data = new Date()
-    let horariohora = data.getHours
-    let horas = String(data.getHours()).padStart(2, '0')
-    let minutos = String(data.getMinutes()).padStart(2, '0')
-    msg.innerHTML = `Agora são ${horas}:${minutos}`
+  adivinha()
+  contagemlick()
+  }
 
-    if (horariohora < 12) {
-        imagem.src = 'https://images.pexels.com/photos/1237119/pexels-photo-1237119.jpeg?auto=compress&cs=tinysrgb&w=600'
-        corpo.style =  'background-image: linear-gradient(45deg,rgb(255, 228, 77),rgb(255, 159, 32))'
+  window.onload = function carregar () {
+      let msg = document.getElementById('msg')
+      let data = new Date()
+      let horariohora = data.getHours()
+      let horas = String(data.getHours()).padStart(2, '0')
+      let minutos = String(data.getMinutes()).padStart(2, '0')
+      msg.innerHTML = `Agora são ${horas}:${minutos}`
+      if (horariohora < 12) {
+        document.getElementById('App').style  = 'background-image : url(https://images.pexels.com/photos/1237119/pexels-photo-1237119.jpeg?auto=compress&cs=tinysrgb&w=600)'
     } else if (horariohora < 18) {
-        imagem.src = 'https://images.pexels.com/photos/48207/sunset-sky-sun-cloud-48207.jpeg?auto=compress&cs=tinysrgb&w=600'
-        corpo.style =  'background-image: linear-gradient(45deg,rgb(233, 150, 27),rgb(238, 62, 9))'
+        document.getElementById('App').style  = 'background-image : url(https://images.pexels.com/photos/48207/sunset-sky-sun-cloud-48207.jpeg?auto=compress&cs=tinysrgb&w=600)'
     } else {
-        imagem.src = 'https://images.pexels.com/photos/771883/pexels-photo-771883.jpeg?auto=compress&cs=tinysrgb&w=600' 
-        corpo.style =  'background-image: linear-gradient(45deg,rgb(121, 186, 216),rgb(7, 53, 97))'
+        document.getElementById('App').style  = 'background-image : url(https://images.pexels.com/photos/771883/pexels-photo-771883.jpeg?auto=compress&cs=tinysrgb&w=600)' 
     }
+
 };
-        const adivinha = () => {
+        function adivinha () {
             let random = (min,max) => Math.floor(Math.random() * (max - min) + min);
             let option1 = document.getElementById('Opcao1')
             let option2 = document.getElementById('Opcao2')
             let res = document.getElementById('res')
-            let input1 = (option1.value)
-            let input2 = (option2.value)
-            let aleatoriedade = [`${input1}`,`${input2}`]
+            let aleatoriedade = [`${option1.value}`,`${option2.value}`]
             let escolhafinal = (aleatoriedade[random(0,aleatoriedade.length)])
             res.innerHTML = `O polvo escolheu a opção : ${escolhafinal}`
         };
-        const contagemlick = () => {
+        function contagemlick () {
             let img = document.getElementById('img')
             numero++
             let pontuacao = document.getElementById("pontuacao")
@@ -55,26 +48,22 @@ function App() {
             }
         };
         return (
-          <div id="App" onLoad={carregar}>
+          <div id="App">
           <div id="primeiraparte" className='primeiraparte'>
           <div id="foto">
-          <img className='imagemaleatoria' id="imagemaleatoria"  src="imagem" alt='imagem' />
           </div>
           <div className='msg' id="msg">Agora são horas</div>
           </div>
           <div id="segundaparte" className='segundaparte'>
           <img className='img' id="img" src="https://pergunteaopolvo.com.br/static/media/polvo.3253b186.webp" alt=""/>     
-          <h1>Escreva duas palavras nos blocos de texto para o polvo escolher</h1>  
+          <p id='paragrafo'>Escreva duas palavras para o polvo escolher</p>  
           <input type="text" id="Opcao1"/> vs.
           <input type="text" id="Opcao2"/> <br/>
           <button type="button" id="botaodeativar" onClick={funcoes}>Adivinhar</button>  
-          <div id="res">Clica no botão de adivinhar</div>
-          <div id="pontuacao" className='final'>Clique no botão de adivinhar</div>
+          <div id="res" className='resultado'>Clica no botão de adivinhar</div>
+          <div id="pontuacao" className='resultado'>Clique no botão de adivinhar</div>
           </div>
           </div>
         );
-}
-
-
-
-export default App;
+};
+        export default App;
